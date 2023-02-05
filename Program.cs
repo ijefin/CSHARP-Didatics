c4f47772425632226ad241c70ef2dec446da3a9a
ijefin <jeffersonmecmiler@hotmail.com>
2023-02-05 15:04:52 -0300

    created models

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DOTNET.Models;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Olá mundo!");
+
+
+Pessoa Jefferson = new Pessoa()
+{
+    Nome = "Jefferson Lucas Gomes",
+    Idade = 25
+
+};
+
+Jefferson.Apresentar();
