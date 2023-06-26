0b4d301472f8c0535d36ec2e199c9fa4e8241f76
ijefiN <jeffersonmecmiler@hotmail.com>
2023-06-26 12:57:27 -0300

    get and set

---
+++ 08_GETTERS_&_SETTERS/Classes/Pessoa.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Pessoa
+{
+    private string nome;
+    private int idade;
+    private string profissao;
+
+    public string Nome
+    {
+        get { return nome; }
+        set { nome = value; }
+    }
+
+    public int Idade
+    {
+        get { return idade; }
+        set { idade = value; }
+    }
+    public string Profissao
+    {
+        get { return profissao; }
+        set { profissao = value; }
+    }
+
+}
---
+++ 08_GETTERS_&_SETTERS/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Pessoa jeff = new Pessoa();
+
+jeff.Nome = "Jefferson L Gomes";
+jeff.Idade = 25;
+jeff.Profissao = "Developer";
+
+Console.WriteLine($"{jeff.Nome}, {jeff.Idade}, {jeff.Profissao}");
