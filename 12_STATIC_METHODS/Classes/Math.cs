208f06e8d74f76dcc0efe17760adab5d29ffb72e
ijefiN <jeffersonmecmiler@hotmail.com>
2023-07-05 17:26:05 -0300

    static methods

---
+++ 12_STATIC_METHODS/Classes/Math.cs
@@ -0,0 +1,19 @@
+class Math
+{
+    public static void soma(int a, int b)
+    {
+        System.Console.WriteLine(a + b);
+    }
+    public static void multiplica(int a, int b)
+    {
+        System.Console.WriteLine(a * b);
+    }
+    public static void divide(int a, int b)
+    {
+        System.Console.WriteLine(a / b);
+    }
+    public static void subtrai(int a, int b)
+    {
+        System.Console.WriteLine(a - b);
+    }
+}
---
+++ 12_STATIC_METHODS/Program.cs
@@ -0,0 +1,4 @@
+﻿Math.soma(2, 2);
+Math.subtrai(5, 2);
+Math.divide(25, 5);
+Math.divide(12, 2);
